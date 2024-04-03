c6fa1bdb6dd66aa1a5e47911ec2594cad46c8b05
sergei_kolbasin <georgio1997@mail.ru>
2024-04-03 21:52:29 +0300

    first test commit

--- Nomia/questionnaire/admin.py
+++ Nomia/questionnaire/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Answer
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["title", "comment", "display_answers"]
+    list_display_links = ["title", "comment"]
+
+    def display_answers(self, obj):
+        return ", ".join([answer.text for answer in obj.answers.all()])
+
+    display_answers.short_description = 'Answers'
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ["text"]
+    list_display_links = ["text"]
--- Nomia/questionnaire/models.py
+++ Nomia/questionnaire/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class Institution(models.Model):
+    """
+    Модель заведения
+    """
+    class Meta:
+        verbose_name_plural = _("Заведения")
+        verbose_name = _("Заведение")
+
+    name = models.CharField(max_length=255, null=False, unique=True, verbose_name=_('Название заведения'))
+    city_country = models.CharField(max_length=255, null=False, verbose_name=_('Страна и город'))
+    address = models.CharField(max_length=200, null=False, unique=True, verbose_name=_('Адрес'))
+    business_type = models.CharField(max_length=50, null=False, verbose_name=_('Тип бизнеса'))
+    direction = models.CharField(max_length=50, null=False, verbose_name=_('Направление'))
+
+
+class Question(models.Model):
+    """
+    Модель вопроса
+    """
+
+    class Meta:
+        verbose_name_plural = _("Вопросы")
+        verbose_name = _("Вопрос")
+
+    title = models.CharField(max_length=255, verbose_name=_('Заголовок'))
+    comment = models.TextField(null=True,verbose_name=_('Дополнительный комментарий'))
+
+    def __str__(self):
+        return f"Вопрос - {self.title}\nКомментарий - {self.comment})"
+
+
+def answer_image_directory_path(instance: "Answer", filename: str) -> str:
+    return "answers_pictures/{filename}".format(
+        answer=instance.text,
+        filename=filename,
+    )
+
+
+class Answer(models.Model):
+    """
+    Модель ответа
+    """
+
+    class Meta:
+        verbose_name_plural = _("Ответы")
+        verbose_name = _("Ответ")
+
+    text = models.CharField(max_length=255)
+    clarification = models.CharField(
+        max_length=255,
+        null=True,
+        verbose_name=_("Уточнение"),
+    )
+    image = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to=answer_image_directory_path,
+        verbose_name=_("Картинка"),
+    )
+
+    def __str__(self):
+        return self.text
+
+class
