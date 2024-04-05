f1dc5631e55067cc357eac7f179096949a82146c
sergei_kolbasin <georgio1997@mail.ru>
2024-04-05 17:11:28 +0300

    fix auth and Makefile

--- Nomia/auth_user/views.py
+++ Nomia/auth_user/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import authenticate
-from django.http import HttpRequest, HttpResponse
+from django.contrib.auth import authenticate, login
+from django.http import HttpRequest
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -9,16 +9,17 @@ def login_view(request: HttpRequest):
         if request.user.is_authenticated:
             return redirect(reverse('questionnaire:enter'))
 
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect(reverse('questionnaire:enter'))
+        else:
+            error_message = "Invalid username or password. Please try again."
+            return render(request, 'auth_user/login.html', {'error_message': error_message})
+
     return render(request, 'auth_user/login.html')
-
-    username = request.POST['username']
-    password = request.POST['password']
-
-    user = authenticate(request, username=username, password=password)
-
-    if user is not None:
-        login(request, user)
-        return redirect(reverse('questionnaire:enter'))
-
-    return BaseException
-
