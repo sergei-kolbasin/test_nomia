753d4f5354afa1a4545cc71d492da7077ec1a7eb
sergei_kolbasin <georgio1997@mail.ru>
2024-04-05 17:16:56 +0300

    add tests for auth_user

--- Nomia/auth_user/tests.py
+++ Nomia/auth_user/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class LoginViewTestCase(TestCase):
+    def test_get_request_authenticated_user(self):
+        response = self.client.get(reverse('auth_user:login_view'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_request_invalid_credentials(self):
+        response = self.client.post(reverse('login_view'), {'username': 'invalid_user', 'password': 'invalid_password'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Invalid username or password. Please try again.")
